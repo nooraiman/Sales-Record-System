a381a2e0a86a8c6c5e14861abce4b6c67a1a5ed9
Aiman <36498944+nooraiman@users.noreply.github.com>
2020-12-22 10:13:20 +0800

    Database Connection
    Connect to Database using PDO

---
+++ includes/database.php
@@ -0,0 +1,17 @@
+<?php
+
+//Database Config
+$db_host = "localhost";
+$db_user = "root";
+$db_password = "";
+$db_name = "srs";
+
+try
+{
+    $conn = new PDO("mysql:host=$db_host;dbname=$db_name",$db_user,$db_password);
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e)
+{
+    echo "Connection failed: " . $e->getMessage();
+}
